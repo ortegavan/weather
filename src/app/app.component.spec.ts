d671b5d670a97e1d1be9b6a3a70446bfe0d34682
Vanessa Ortega <ortegavan@live.com>
2024-05-19 23:23:22 -0300

    chore: aplicação criada

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,27 @@
+import { TestBed, waitForAsync } from '@angular/core/testing'
+import { getNativeElementByTestId } from 'angular-unit-test-helper'
+import { MockComponents } from 'ng-mocks'
+
+import { AppComponent } from './app.component'
+import { CurrentWeatherComponent } from './current-weather/current-weather.component'
+
+describe('AppComponent', () => {
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [AppComponent, ...MockComponents(CurrentWeatherComponent)],
+    }).compileComponents()
+  }))
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent)
+    const app = fixture.componentInstance
+    expect(app).toBeTruthy()
+  })
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent)
+    fixture.detectChanges()
+    const titleElement = getNativeElementByTestId(fixture, 'title')
+    expect(titleElement.textContent).toContain('LocalCast Weather')
+  })
+})
---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <div>
+      <mat-toolbar color="primary">
+        <span data-testid="title">LocalCast Weather</span>
+      </mat-toolbar>
+      <div fxLayoutAlign="center">
+        <div class="mat-caption v-pad">
+          Your city, your forecast, right now!
+        </div>
+      </div>
+      <div fxLayout="row">
+        <div fxFlex></div>
+        <mat-card appearance="outlined" fxFlex="300px">
+          <mat-card-header>
+            <mat-card-title>
+              <div class="mat-headline-5">Current Weather</div>
+            </mat-card-title>
+          </mat-card-header>
+          <mat-card-content>
+            <app-current-weather></app-current-weather>
+          </mat-card-content>
+        </mat-card>
+        <div fxFlex></div>
+      </div>
+    </div>
+  `,
+  standalone: true,
+  imports: [CurrentWeatherComponent, MatToolbarModule, MatCardModule],
+})
+export class AppComponent {}
