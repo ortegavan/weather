fb1cd7982f11fc1527c8f431d3e15d2b996f7a45
Vanessa Ortega <ortegavan@live.com>
2024-05-21 09:53:49 -0300

    feat: criado componente para busca de localidade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,18 @@ import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+import { CitySearchComponent } from './city-search/city-search.component';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
     standalone: true,
-    imports: [CurrentWeatherComponent, MatToolbarModule, MatCardModule],
+    imports: [
+        CurrentWeatherComponent,
+        MatToolbarModule,
+        MatCardModule,
+        CitySearchComponent,
+    ],
 })
 export class AppComponent {}
---
+++ src/app/city-search/city-search.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, inject } from '@angular/core';
+import {
+    FormControl,
+    FormsModule,
+    ReactiveFormsModule,
+    Validators,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { WeatherService } from '../weather/weather.service';
+import { debounceTime } from 'rxjs';
+
+@Component({
+    selector: 'app-city-search',
+    standalone: true,
+    imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatIconModule,
+    ],
+    templateUrl: './city-search.component.html',
+    styleUrl: './city-search.component.css',
+})
+export class CitySearchComponent implements OnInit {
+    service = inject(WeatherService);
+    search = new FormControl('', [Validators.minLength(2)]);
+
+    ngOnInit(): void {
+        this.search.valueChanges.pipe(debounceTime(1000)).subscribe((value) => {
+            if (!this.search.invalid && value) {
+                const input = value.split(',').map((s: string) => s.trim());
+                this.service
+                    .getCurrentWeather(
+                        input[0],
+                        input.length > 1 ? input[1] : undefined,
+                    )
+                    .subscribe((weather) => {
+                        console.log(weather);
+                    });
+            }
+        });
+    }
+}
